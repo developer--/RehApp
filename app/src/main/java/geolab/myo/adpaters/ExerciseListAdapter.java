5b2959a0f84d3247e22d09c1425fce6038a622e5
developer-- <jemo.mgebrishvili@gmail.com>
2015-09-26 23:46:38 +0400

    Added Model Exercise

---
+++ app/src/main/java/geolab/myo/adpaters/ExerciseListAdapter.java
@@ -0,0 +1,40 @@
+package geolab.myo.adpaters;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+
+import java.util.ArrayList;
+
+import geolab.myo.model.Exercises;
+
+public class ExerciseListAdapter extends BaseAdapter {
+
+    private ArrayList<Exercises> myoTutsArrayList;
+    private Context context;
+    private LayoutInflater inflater;
+
+
+
+    @Override
+    public int getCount() {
+        return 0;
+    }
+
+    @Override
+    public Object getItem(int i) {
+        return null;
+    }
+
+    @Override
+    public long getItemId(int i) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int i, View view, ViewGroup viewGroup) {
+        return null;
+    }
+}
---
+++ app/src/main/java/geolab/myo/model/Exercises.java
@@ -0,0 +1,22 @@
+package geolab.myo.model;
+
+
+public class Exercises {
+
+    private long duration;
+    private String name;
+    boolean status;
+
+    public enum ExerciseTypes {
+        FINGERS_SPREAD,
+        FIST,
+        REST,
+        UNKNOWN,
+        DOUBLE_TAP,
+        WAVE_IN,
+        WAVE_OUT
+    }
+
+
+
+}
